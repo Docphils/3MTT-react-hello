4b798c3cfbb74621347a0a50219d854ad1be1fe9
Philip Nwachukwu <docphils64@gmail.com>
2025-06-16 14:51:34 +0100

    Made a click counter app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,37 @@
 import "./App.css";
+import {useState} from "react";
 
 function App() {
+    let [number, setNumber] = useState(0);
+
+    const increment = () => {
+        if (number < 10) {
+            setNumber(number + 1);
+        } else {
+            alert("You have reached the max number of 10");
+        }
+    };
+
+    const decrement = () => {
+        if (number > 0) {
+            setNumber(number - 1);
+        } else {
+            alert("You cannot decrement below 0");
+        }
+    };
+    //const reset = () => {setNumber(0);};
+
     return (
         <div className='App'>
-            <h1>Hello World</h1>
+            <h1>Docphil's Click Counter</h1>
+            <h3>{number}</h3>
+            <div className='buttons'>
+                <button onClick={() => decrement()}>-</button>
+                <button onClick={() => increment()}>+</button>
+            </div>
+            <button onClick={() => setNumber(0)} className='reset'>
+                Reset
+            </button>
         </div>
     );
 }
